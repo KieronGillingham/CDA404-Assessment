d9440a8f23c9f62aa50e70b89dffd1cd0c4f63e7
Kieron Gillingham <kierongillingham@gmail.com>
2019-04-12 11:55:15 +0100

    Initialized signinRedirect.js

--- deployment/scripts/signinRedirect.js
+++ deployment/scripts/signinRedirect.js
@@ -1,11 +1,5 @@
-function onSignIn(googleUser)
+function onSignIn()
 {
-    // Useful data for your client-side scripts:
-    // var profile = googleUser.getBasicProfile();
-
-    // The ID token you need to pass to your backend:
-    var id_token = googleUser.getAuthResponse().id_token;
-
     //if (id_token != null)
     //    window.location.href = "/user_dash.html";
     //else
